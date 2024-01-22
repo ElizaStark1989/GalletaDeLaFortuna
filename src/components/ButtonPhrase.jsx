37deb9ad1c96b6d476e2762ea3e33f4e7d3c2dd8
Elizabeth <elizastark-1989@outlook.es>
2024-01-22 18:17:25 -0500

    Agregando estilos

--- src/components/ButtonPhrase.jsx
+++ src/components/ButtonPhrase.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import phrases from '../utils/phrases.json';
 import randomIndex from '../services/randomIndex';
+import bgArray from '../utils/bgArray.json';
 
-const ButtonPhrase = ({setquote}) => {
+const ButtonPhrase = ({setquote, setBgApp}) => {
 
    const sentence = randomIndex(phrases);
 
     const handleButton = () => {
+        const bgIndex = randomIndex(bgArray);
         const sentence = randomIndex(phrases);
         setquote(sentence);
+        setBgApp(bgIndex);
+   
     }
 
   return (
