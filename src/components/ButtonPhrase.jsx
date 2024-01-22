a8ef7a0077b05e6ac5a01940261a2797d05a12cf
ElizaStark1989 <153787420+ElizaStark1989@users.noreply.github.com>
2024-01-22 11:58:40 -0500

    Revert "Agregando fondos al proyecto"

--- src/components/ButtonPhrase.jsx
+++ src/components/ButtonPhrase.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import phrases from '../utils/phrases.json';
 import randomIndex from '../services/randomIndex';
-import bgArray from '../utils/bgArray.json';
 
-const ButtonPhrase = ({ setquote, setBgApp }) => {
+const ButtonPhrase = ({setquote}) => {
 
-    const sentence = randomIndex(phrases);
+   const sentence = randomIndex(phrases);
 
     const handleButton = () => {
-        const bgIndex = randomIndex(bgArray);
         const sentence = randomIndex(phrases);
         setquote(sentence);
-        setBgApp(bgIndex);
     }
 
-    return (
-        <button onClick={handleButton}>Probar mi suerte</button>
-    )
+  return (
+    <button onClick={handleButton}>Probar mi suerte</button>
+  )
 }
 
 export default ButtonPhrase;
